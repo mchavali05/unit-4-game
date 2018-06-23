7a3109ecbca372eb8c2ec219cc8d3480ac46755c
Madhu <mchavali05@gmail.com>
2018-06-23 01:28:42 -0700

    working pieces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,17 @@
 //We will have a number with starting value of 0, this number will be incremented by the random number associated with the crystal that was clicked.
 
 var crystals = ["http://www.scienceclarified.com/photos/crystal-3107.jpg", "http://www.scienceclarified.com/photos/crystal-3107.jpg", "http://www.scienceclarified.com/photos/crystal-3107.jpg", "http://www.scienceclarified.com/photos/crystal-3107.jpg"];
-var startingNumber = 19;
-var goalNumber = 120;
+var startingNumber = 0;
+//var goalNumber = 120;
 var winNumber = 0;
 var lossNumber = 0;
-$('#starting-number').text(startingNumber);
-$('#goal-number').text(goalNumber);
+//var score = 0;
+
+//$('#starting-number').text(startingNumber);
+//$('#goal-number').text(goalNumber);
 $('#win-number').text(winNumber);
 $('#loss-number').text(lossNumber);
+$('#score').text(startingNumber);
 
 
 for(i=0; i<crystals.length; i++) {
@@ -22,30 +25,62 @@ for(i=0; i<crystals.length; i++) {
 
 }
 
+function randomIntFromInterval(min,max){
+		return Math.floor(Math.random()*(max-min+1)+min);
+		}
+		
+var randomNumber = randomIntFromInterval(19,120);
+console.log(randomNumber);
+
+// Function that resets the game.
+	function resetGame() {
+    // Make our current total number 0.
+    startingNumber = 0;
+    $('#score').text(startingNumber);
+   
+    // Generate a random target number and render it to the page.
+    randomNumber = randomIntFromInterval(19,120);
+	console.log(randomNumber);
+	$('#rand-number').text(randomNumber);
+
+  }
+
 $('.crystal-button').on('click', function(){
+	
 	var number = parseInt($(this).attr('info'));
 	console.log(number);
 	startingNumber = startingNumber + number;
 	console.log(startingNumber);
+		 
 	$('#starting-number').text(startingNumber);
-	$('#goal-number').text(goalNumber);
-	if(startingNumber>goalNumber) {
+	//$('#goal-number').text(goalNumber);
+	$('#score').text(startingNumber);
+	$('#rand-number').text(randomNumber);
+  
+
+	if(startingNumber>randomNumber) {
 		lossNumber++;
+		$('#won-or-lost').text('You Lost!!');
 		console.log(lossNumber);
 		$('#loss-number').text(lossNumber);
+		resetGame();
 		setTimeout(function(){
 			alert("you lost");
 		}, 100)
-		//comment
+	
+
 	}
-	if(startingNumber==goalNumber) {
+	if(startingNumber==randomNumber) {
 		winNumber++;
+		$('#won-or-lost').text('You Won!!');
 		console.log(winNumber);
 		$('#win-number').text(winNumber);
+		resetGame();
 		setTimeout(function(){
 			alert("you won!");
 		}, 100)
 		
-	}
+	}	
+
 
 }) 
